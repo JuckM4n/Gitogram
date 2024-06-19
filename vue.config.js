f2013c6b76d37740a06929dab2e7e0ced8afe76f
JuckM4n <nik_morozov0@mail.ru>
2024-06-19 12:07:43 +0300

    fix: try to fix ghp

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,3 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true,
-  publicPath: '/gitogram/dist/',
-})
+module.exports = {
+  publicPath: '/gitogram/dist/'
+}
